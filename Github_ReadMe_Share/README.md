804317cbfc24fc3d85176681c55830550a090b7e
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-07-21 12:53:51 -0500

    the start

--- Github_ReadMe_Share/README.md
+++ Github_ReadMe_Share/README.md
@@ -1,4 +1,4 @@
-<h1 align="center"><img src="../shared/img/header.png" style="width: 100%; max-width: 400px;"/></h1>
+<h1 align="center"><img src="https://github.com/clarkhacks/100Concepts/raw/main/shared/img/header.png" style="width: 100%; max-width: 400px;"/></h1>
 <h3 align="center">I have at max 200 days to publish 100 of my application concepts. Some are fancier than others, join me on my journey to become a better developer.</h3>
 
 ### Github Readme Share
--- Github_ReadMe_Share/index.js
+++ Github_ReadMe_Share/index.js
@@ -1,17 +1,44 @@
 // express app
 const express = require('express');
 const pug = require('pug');
+const axios = require('axios');
+var md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typographer: true
+  });
 const app = express();
 const port = 3000;
 // use pug
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', './views');
 //static files
 app.use(express.static('public'));
 // routes
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/:user/:repo', (req, res) => {
+    //axios get contents of remote readme
+    axios.get(`https://raw.githubusercontent.com/${req.params.user}/${req.params.repo}/main/README.md`)
+        .then(response => {
+            //convert markdown data to html
+            var html = md.render(response.data);
+            //render readme
+            res.render('readme', {
+                readme: html,
+                user: req.params.user,
+                repo: req.params.repo
+            });
+        }).catch(err => {
+            res.render('index', {
+                error: err
+            });
+            console.log(err);
+
+        });
+});
+
 // express listen
 app.listen(port, () => {
     console.log(`Example app listening on ${port}`);
