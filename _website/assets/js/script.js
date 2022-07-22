c998cfefab8ef1201f412dd12cd17eb8c373d0a2
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-07-22 09:46:52 -0500

    timer runs on load then every minute

--- _website/assets/js/script.js
+++ _website/assets/js/script.js
@@ -1,14 +1,19 @@
 var deadline = new Date("Feb 6, 2023 11:30:00").getTime();
+
+function updateTimer() {
+    var now = new Date().getTime();
+    var t = deadline - now;
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    document.getElementById("countdown").innerHTML =
+        days + " Days " + hours + " Hours &amp; " + minutes + " Minutes Left ";
+    if (t < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}
+updateTimer();
 var x = setInterval(function () {
-  var now = new Date().getTime();
-  var t = deadline - now;
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-  document.getElementById("countdown").innerHTML =
-    days + " Days " + hours + " Hours &amp; " + minutes + " Minutes Left ";
-  if (t < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
+    updateTime();
 }, 1000 * 60);
