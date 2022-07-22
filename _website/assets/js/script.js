5a5b62e5ec9b8fbc7bed96629dabfa3da950aaee
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-07-22 09:38:40 -0500

    added timer

--- _website/assets/js/script.js
+++ _website/assets/js/script.js
@@ -1,13 +1,15 @@
-const instance = ackeeTracker.create('https://ackee.weckmann.me/');
-document.querySelector('#ackee_devto').addEventListener('click', () => {
-    instance.action('355d6bb5-af12-498c-bd8a-38160150ea69', {
-        key: 'Click',
-        value: 1
-    })
-})
-document.querySelector('#ackee_devto').addEventListener('mousover', () => {
-    instance.action('355d6bb5-af12-498c-bd8a-38160150ea69', {
-        key: 'Hovered',
-        value: 1
-    })
-})
+var deadline = new Date("Feb 6, 2023 11:30:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var t = deadline - now;
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((t % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML =
+    days + " Days " + hours + " Hours " + minutes + " Minutes &amp; " + seconds + " Seconds Left ";
+  if (t < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
