84516316ad2259639f6a69cca83d2f2a21c34b13
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-07-23 14:42:26 -0500

    Dashboard: Loading Buttons Works

--- Dashboard/assets/breadly.js
+++ Dashboard/assets/breadly.js
@@ -49,3 +49,21 @@ function getCurrentTime() {
 getTodaysDate();
 //update time every second
 setInterval(getCurrentTime, 1000);
+
+//get json from https://api.npoint.io/6f845374e05d1f612cd3 and create list item for each item in the links
+function getLinks() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.npoint.io/6f845374e05d1f612cd3", true);
+    xhr.onload = function () {
+        if (this.status == 200) {
+            const links = JSON.parse(this.responseText).links;
+            links.forEach(link => {
+                const li = document.createElement("li");
+                li.innerHTML = '<a href="' + link.href + '" target="' + link.target + '" class="button n01"><i class="' + link.icon + '"></i><span class="label">' + link.title + '</span></a>';
+                document.getElementById("buttons01").appendChild(li);
+            });
+        }
+    };
+    xhr.send();
+}
+getLinks();
